2f583cbd58f5748b82df001490bb3576f7026436
RupasreeH <hroopa015@gmail.com>
2025-04-09 12:04:47 -0500

    Updating suggestions logic

--- fe-risk-assessment/components/Suggestions.tsx
+++ fe-risk-assessment/components/Suggestions.tsx
@@ -2,53 +2,63 @@ import { View, StyleSheet } from "react-native";
 import React, { useEffect, useState } from "react";
 import Typo from "./Typo";
 import { useAuth } from "@/context/authContext";
-import { PII_Weight, PII_Willingness } from "@/constants/piiScores";
+import {
+  beta_coefficients,
+  resolution_powers,
+  weights,
+  willingness_measures,
+} from "@/constants/piiScores";
 import { colors } from "@/constants/theme";
 
 const Suggestions = () => {
   const { results }: any = useAuth();
-  const [informationText, setInformationText] = useState<string[]>([]);
+  const [informationText, setInformationText] = useState<any[]>([]);
   const levels = ["very low", "low", "medium", "high", "critical"];
   const [nextLevel, setNextLevel] = useState<string>(
     levels[levels.indexOf(results.risk_level.toLowerCase()) + 1]
   );
+
+  const calculatePrivacyScore = (
+    willingnessMeasure: number,
+    resolutionPower: number,
+    betaCoefficient: number
+  ) => {
+    const privacyScore =
+      1 /
+      Math.exp(betaCoefficient * (1 - willingnessMeasure) * resolutionPower);
+    return privacyScore;
+  };
+
   useEffect(() => {
     if (!results) return;
     const emptyFields = Object.keys(results).filter(
       (key: string) => results[key]?.length === 0
     );
-    const willingnessObject = Object.keys(PII_Willingness).filter(
-      (key: string) => emptyFields.find((efkey) => efkey === key)
-    );
-    let currentRiskScore = results.risk_score;
-    let information: string[] = [];
-    for (const key of willingnessObject) {
-      const weight = PII_Weight[key as keyof typeof PII_Weight];
-      currentRiskScore = currentRiskScore + weight;
-      information.push(key);
-
-      if (
-        nextLevel === "very low" &&
-        currentRiskScore > 2.74 &&
-        currentRiskScore <= 5.58
-      ) {
-        break;
-      } else if (
-        nextLevel === "low" &&
-        currentRiskScore > 5.58 &&
-        currentRiskScore <= 6.87
-      ) {
-        break;
-      } else if (
-        nextLevel === "medium" &&
-        currentRiskScore > 6.87 &&
-        currentRiskScore <= 12.25
-      ) {
-        break;
-      } else if (nextLevel === "high" && currentRiskScore > 12.25) {
-        break;
+    let information: any[] = [];
+    emptyFields.forEach((key: string) => {
+      if (key === "Phone") {
+        return;
       }
-    }
+      const weight = weights[key as keyof typeof weights];
+      const willingness =
+        willingness_measures[key as keyof typeof willingness_measures];
+      const resolutionPower =
+        resolution_powers[key as keyof typeof resolution_powers]; // Assuming a constant resolution power for simplicity
+      const betaCoefficient =
+        beta_coefficients[key as keyof typeof beta_coefficients]; // Assuming a constant beta coefficient for simplicity
+      const privacyScore = calculatePrivacyScore(
+        willingness,
+        resolutionPower,
+        betaCoefficient
+      );
+      console.log(`${key}`, privacyScore);
+      console.log(`${key}`, weight * privacyScore);
+      information.push({
+        key: key,
+        score: weight * privacyScore,
+      });
+    });
+
     setInformationText(information);
   }, [results]);
   return (
@@ -62,28 +72,21 @@ const Suggestions = () => {
       </Typo>
       <Typo
         size={14}
-        fontWeight={"700"}
+        fontWeight={"500"}
         style={{
           marginBottom: 10,
         }}
       >
-        Your risk score might increase to{" "}
-        <Typo
-          size={14}
-          fontWeight={"700"}
-          style={{
-            color: colors[nextLevel.toLowerCase() as keyof typeof colors],
-          }}
-        >
-          {nextLevel}
-        </Typo>{" "}
-        if you expose below information.
+        Disclosing any of the following, along with their individual risk
+        scores, may elevate your risk profile.
       </Typo>
       <View style={{ flexDirection: "column", flexWrap: "wrap" }}>
         {informationText.length > 0 &&
           informationText.map((item: string, index: number) => (
             <View
               style={{
+                display: "flex",
+                flexDirection: "row",
                 borderWidth: 0,
                 width: "100%",
                 marginBottom: 5,
@@ -91,8 +94,11 @@ const Suggestions = () => {
               }}
               key={index}
             >
-              <Typo size={12} style={{ marginRight: 5 }} key={index}>
-                {informationText[index]}
+              <Typo size={12} fontWeight={"700"} style={{ marginRight: 5 }}>
+                {`${informationText[index].key} : `}
+              </Typo>
+              <Typo size={12} style={{ marginRight: 5 }}>
+                {informationText[index].score.toFixed(2)}
               </Typo>
             </View>
           ))}
